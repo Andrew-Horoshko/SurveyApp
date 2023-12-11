901898cba7f895896a769753534d2cd931c25082
Andrew-Horoshko <safer.andy@gmail.com>
2023-12-11 23:43:49 +0200

    Added view model

---
+++ SurveyAppServer/API/View Models/SurveyRatingViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SurveyAppServer.View_Models
+{
+    public class SurveyRatingViewModel
+    {
+        public int SurveyRatingId { get; set; }
+        public int SurveyId { get; set; }
+        public int UserId { get; set; }
+        public int Mark { get; set; }
+    }
+}
