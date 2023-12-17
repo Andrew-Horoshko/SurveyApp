e68e45ccd0aa222339a03011d7c785b58908779e
Oleh Sivak <olegsivak.2003@gmail.com>
2023-12-17 21:26:51 +0100

    Update API URL

--- SurveyAppClient/surveyapp/src/services/Answer.ts
+++ SurveyAppClient/surveyapp/src/services/Answer.ts
@@ -2,7 +2,7 @@ import api from './api'
 
 export const getAnswersForQuestion = async (questionId: number) => {
     try {
-        const response = await api.get(`/Answers?questionId=${questionId}`);
+        const response = await api.get(`/api/Question/Answers?questionId=${questionId}`);
         return response.data; 
     } catch (error) {
         throw new Error('Failed to fetch survey questions: ' + error.message);
