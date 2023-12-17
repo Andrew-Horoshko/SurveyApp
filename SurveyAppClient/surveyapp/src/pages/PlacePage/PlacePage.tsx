e7f994a0d0fa7d032468cbd70203c2b8593611e3
MeteorPro <a.kruba.a@gmail.com>
2023-12-17 23:05:15 +0200

    fix choice of answers

--- SurveyAppClient/surveyapp/src/pages/PlacePage/PlacePage.tsx
+++ SurveyAppClient/surveyapp/src/pages/PlacePage/PlacePage.tsx
@@ -12,127 +12,124 @@ export const PlacePage: React.FC = () => {
     const { id } = useParams<{ id: string }>();
     const [questions, setQuestions] = useState<any[]>([]);
     const [answers, setAnswers] = useState<any[]>([]);
-
     const [userAnswers, setUserAnswers] = useState<SurveyAnswer[]>([]);
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-
-        // Parse data from input name
-        const { name, value } = e.target;
-        const identifiers: string[] = name.split(/[q_a]+/);
-        console.log(name);
-        console.log(identifiers);
-        const questionType: string = identifiers[0];
-        const questionId: number = parseInt(identifiers[1]);
-        const answerId: number = parseInt(identifiers[2]);
-
-        // Create new user answer entry
-        const userAnswer: SurveyAnswer = {
-            surveyAnswerId: 0,
-            surveyAttemptId: 0,
-            questionId: questionId,
-            answerId: answerId,
-        };
-        if (value) {
-            userAnswer.openAnswer = value;
-        }
-
-        // Update user answers state
-        setUserAnswers(prevData => {
-
-            // Handle checkbox input
-            if (questionType == 'm') {
-                const prevAnswerIndex = prevData.findIndex(a => 
-                    a.questionId == userAnswer.questionId && 
-                    a.answerId == userAnswer.answerId);
-                if (prevAnswerIndex == -1) {
-                    prevData.push(userAnswer);
-                }
-
-                return prevData;
-            }
-
-            // Handle radiobutton/text input
-            const prevAnswerIndex = prevData.findIndex(a => a.questionId == userAnswer.questionId);
-            if (prevAnswerIndex != -1) {
-                prevData.splice(prevAnswerIndex, 1);
-            }
-            prevData.push(userAnswer);
-
-            return prevData;
-        });
-
-        console.log(userAnswers);
-    }
-
-    function submitSurvey() {
-        const surveyAttempt: SurveyAttempt = {
-            surveyAttemptId: 0,
-            userId: 1, // TODO: pass actual user id
-            surveyId: id,
-            surveyAnswers: userAnswers
-        };
-
-        submitSurveyAttempt(surveyAttempt);
-    }
-
     useEffect(() => {
-        async function fetchSurvey() {
+        const fetchSurvey = async () => {
             try {
                 const survey = await getSurvey(parseInt(id));
                 setSurvey(survey);
             } catch (error) {
                 console.error("Error while fetching survey", error);
             }
-        }
-
+        };
         fetchSurvey();
-    }, [survey]);
+    }, [id]);
 
     useEffect(() => {
-        async function fetchQuestions() {
+        const fetchQuestions = async () => {
             try {
                 const data = await getSurveyQuestions(parseInt(id));
                 setQuestions(data);
             } catch (error) {
                 console.error('Error fetching questions:', error);
             }
-        }
-
+        };
         fetchQuestions();
     }, [id]);
 
     useEffect(() => {
-        async function fetchAnswers(questionId: number) {
+        const fetchAnswers = async () => {
             try {
-                const data = await getAnswersForQuestion(questionId);
-                setAnswers(prevAnswers => [...prevAnswers, ...data]);
+                const fetchedAnswers: any[] = [];
+                for (const question of questions) {
+                    const answersForQuestion = await getAnswersForQuestion(question.questionId);
+                    fetchedAnswers.push(...answersForQuestion);
+                }
+                setAnswers(fetchedAnswers);
             } catch (error) {
-                console.error('Error fetching answers for question:', error);
+                console.error('Error fetching answers:', error);
             }
-        }
-    
-        questions.forEach((question: any) => {
-            fetchAnswers(question.questionId);
-        });
+        };
+        fetchAnswers();
     }, [questions]);
 
+    const handleChange = (
+        e: React.ChangeEvent<HTMLInputElement>,
+        questionId: number,
+        answerId: number,
+        questionType: string
+    ) => {
+        const { value, checked } = e.target;
+
+        const userAnswer: SurveyAnswer = {
+            surveyAnswerId: 0,
+            surveyAttemptId: 0,
+            questionId: questionId,
+            answerId: answerId,
+        };
+
+        if (value && questionType !== 'MultipleChoice') {
+            userAnswer.openAnswer = value;
+        }
+
+        setUserAnswers(prevAnswers => {
+            const updatedAnswers = [...prevAnswers.filter(a => a.questionId !== questionId)];
+
+            if (checked || value) {
+                updatedAnswers.push(userAnswer);
+            }
+
+            return updatedAnswers;
+        });
+    };
+
+    const submitSurvey = () => {
+        const surveyAttempt: SurveyAttempt = {
+            surveyAttemptId: 0,
+            userId: 1, // Replace with actual user ID
+            surveyId: id,
+            surveyAnswers: userAnswers,
+        };
+
+        submitSurveyAttempt(surveyAttempt)
+            .then((response) => {
+                // Handle success response
+            })
+            .catch((error) => {
+                console.error('Error submitting survey attempt:', error);
+            });
+    };
+
     const renderQuestion = (question: any) => {
-        let questionAnswers = answers.filter((answer: any) => answer.questionId === question.questionId);
-        
+        let questionAnswers = answers.filter(
+            (answer: any) => answer.questionId === question.questionId
+        );
+
         switch (question.questionType) {
             case 'SingleChoice':
                 return (
                     <div key={question.questionId} className="question-container">
                         <div className="question-text">
                             <span>{question.text}</span>
-                            <Hint  hint={question.tooltip} />
+                            <Hint hint={question.tooltip} />
                         </div>
                         <div className="answers">
                             {questionAnswers.map((answer: any) => (
                                 <label key={answer.answerId} className="answer-label">
-                                    <input type="radio" name={`s_q${question.questionId}_a${answer.answerId}`} value={answer.text} 
-                                        onChange={handleChange} />
+                                    <input
+                                        type="radio"
+                                        name={`s_q${question.questionId}`}
+                                        value={answer.text}
+                                        onChange={(e) =>
+                                            handleChange(
+                                                e,
+                                                question.questionId,
+                                                answer.answerId,
+                                                question.questionType
+                                            )
+                                        }
+                                    />
                                     <span className="answer-text">{answer.text}</span>
                                 </label>
                             ))}
@@ -144,13 +141,24 @@ export const PlacePage: React.FC = () => {
                     <div key={question.questionId} className="question-container">
                         <div className="question-text">
                             <span>{question.text}</span>
-                            <Hint  hint={question.tooltip} />
+                            <Hint hint={question.tooltip} />
                         </div>
                         <div className="answers">
                             {questionAnswers.map((answer: any) => (
                                 <label key={answer.answerId} className="answer-label">
-                                    <input type="checkbox" name={`m_q${question.questionId}_a${answer.answerId}`} value={answer.text} 
-                                        onChange={handleChange} />
+                                    <input
+                                        type="checkbox"
+                                        name={`m_q${question.questionId}_a${answer.answerId}`}
+                                        value={answer.text}
+                                        onChange={(e) =>
+                                            handleChange(
+                                                e,
+                                                question.questionId,
+                                                answer.answerId,
+                                                question.questionType
+                                            )
+                                        }
+                                    />
                                     <span className="answer-text">{answer.text}</span>
                                 </label>
                             ))}
@@ -162,10 +170,21 @@ export const PlacePage: React.FC = () => {
                     <div key={question.questionId} className="question-container">
                         <div className="question-text">
                             <span>{question.text}</span>
-                            <Hint  hint={question.tooltip} />
+                            <Hint hint={question.tooltip} />
                         </div>
-                        <input type="text" name={`o_q${question.questionId}_a1`} className="open-answer-input" 
-                            onChange={handleChange} />
+                        <input
+                            type="text"
+                            name={`o_q${question.questionId}_a1`}
+                            className="open-answer-input"
+                            onChange={(e) =>
+                                handleChange(
+                                    e,
+                                    question.questionId,
+                                    1,
+                                    question.questionType
+                                )
+                            }
+                        />
                     </div>
                 );
             default:
@@ -190,3 +209,4 @@ export const PlacePage: React.FC = () => {
         </>
     );
 };
+
